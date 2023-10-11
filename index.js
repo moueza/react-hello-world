a79ebdf9e49db0e5df27703ba4f1b2e952b87f4c
Peter MOUEZA <mouezapeter.2@gmail.com>
2023-10-11 07:59:16 +0200

    feat: js link

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/*
+function changeColor() {
+  document.getElementById("my-div").style.backgroundColor = "green";
+}
+*/
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<h1>Hello, world!</h1>);
